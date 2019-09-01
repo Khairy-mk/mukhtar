42d2c830889937b74b183fb0c64df02a5a7fb887
unknown <khairmukhtie@gmail.com>
2019-09-01 10:59:03 -0600

    first commit

---
+++ plunk/script.js
@@ -0,0 +1,21 @@
+// Code goes here
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.4.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBbzsoXlBL-QNQA4IjRlBA3lGiRxmHkPRk",
+    authDomain: "ummul-58047.firebaseapp.com",
+    databaseURL: "https://ummul-58047.firebaseio.com",
+    projectId: "ummul-58047",
+    storageBucket: "ummul-58047.appspot.com",
+    messagingSenderId: "209871785657",
+    appId: "1:209871785657:web:a476c20805416355"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
